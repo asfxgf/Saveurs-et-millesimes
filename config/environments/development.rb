272457dd04c90684e54691502e3052f5cbeeb78b
quentin deleglise <deleglise.quentin@hotmail.fr>
2020-07-08 01:29:32 +0200

    correction syntax error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,3 +64,4 @@
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors= true
   config.action_mailer.delivery_method = :smtp
@@ -105,5 +104,4 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
-
 end
