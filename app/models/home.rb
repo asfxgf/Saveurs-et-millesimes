175ae1e2674474231ef44e63cf0db7c062edcb30
quentin deleglise <deleglise.quentin@hotmail.fr>
2020-07-08 11:50:10 +0200

    modif mineurs model home

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,10 +1,10 @@
 # app/models/home.rb
 
 class Home < MailForm::Base
-  attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :name, :validate => true
+  attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
-  attribute :nickname,  :captcha  => true
+  attribute :nickname, :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
