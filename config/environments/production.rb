ba9f821d871ebb25ecd8d81ae6f23281245543aa
quentin deleglise <deleglise.quentin@hotmail.fr>
2020-07-09 00:15:36 +0200

    changement production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://saveurs-et-millesimes.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "2leglise.quentin@gmail.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
