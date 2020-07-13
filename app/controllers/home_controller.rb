fb9b9166db29b7d8e4b466554941674bf0c8339b
quentin deleglise <deleglise.quentin@hotmail.fr>
2020-07-13 15:41:33 +0200

    contact form ajax fonctionnel

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,13 +10,13 @@ class HomeController < ApplicationController
     @contact.request = request
     respond_to do |format|
       if @contact.deliver
-        # re-initialize Home object for cleared form
+        # reinitialise pour le form contact
         @contact = Home.new
         format.html { render 'index'}
-        format.js   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
+        format.js   { flash.now[:success] = @message = "Merci pour votre message, nous reviendrons vers vous rapidement !" }
       else
         format.html { render 'index' }
-        format.js   { flash.now[:error] = @message = "Message did not send." }
+        format.js   { flash.now[:error] = @message = "Message non-envoyé" }
       end
     end
   end
